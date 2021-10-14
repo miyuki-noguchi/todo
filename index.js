1c0acdaf0599bb7a4956f6eef0a45cabba4202a4
miyuki-noguchi <miy0916uki@gmail.com>
2021-10-14 21:00:32 +0900

    modal

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-'use strict';
-import { App } from './src/App.js';
-const app = new App();
-app.mount();
+$(function () {
+  $('.btn').each(function () {
+    $(this).click(function () {
+      const target = $(this).data('btn');
+      const modal = document.getElementById(target);
+      $(modal).fadeIn();
+      return false;
+    });
+    $('.hide').click(function () {
+      $(this).fadeOut();
+      return false;
+    });
+  });
+});
