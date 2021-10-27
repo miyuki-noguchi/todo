3422b3917f8bedfab103528437142f136655ca8a
miyuki-noguchi <miy0916uki@gmail.com>
2021-10-27 22:20:26 +0900

    kousin

--- index.js
+++ index.js
@@ -17,15 +17,26 @@ $(function () {
       return false;
     });
   });
+
+  const target = $('#target-kawauso1');
+  setInterval(function () {
+    target.addClass('active');
+    target2.removeClass('active');
+  }, 5000);
+
+  const target2 = $('#target-kawauso2');
+  setInterval(function () {
+    target2.addClass('active');
+    target.removeClass('active');
+  }, 7000);
 });
 
-const target = $('#target-kawauso1');
-const el = target.offset().top;
-console.log(el);
-$(window).scroll(function () {
-  const scroll = $(this).scrollTop();
-  console.log(scroll);
-  if (scroll + 800 > el) {
-    target.addClass('active');
-  }
-});
+// const target = $('#target-kawauso1');
+// const el = target.offset().top;
+// $(window).scroll(function () {
+//   const scroll = $(this).scrollTop();
+//   console.log(scroll);
+//   if (scroll + 800 > el) {
+//     target.addClass('active');
+//   }
+// });
